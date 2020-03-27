72648a8fd2a331f12dd60dd7a1290f08eeceaf9e
zhanjixun <zhanjixun@qq.com>
2020-03-27 11:06:30 +0800

    脚本提交

--- push.sh
+++ push.sh
@@ -2,6 +2,8 @@ set -e
 
 set -v
 
+##########默认修改都是在master做的
+
 rm -rf .idea
 
 rm -rf node_modules
@@ -15,3 +17,26 @@ git add -A
 git commit -m '脚本提交'
 
 git push
+
+#########################
+
+#切回主干
+git checkout master
+#先提交主干内容
+#sh push.sh
+#强制复制一条分支出来
+git branch -C deploy
+#切换到发布分支
+git checkout deploy
+#构建博客
+npm run docs:build
+#删除构建缓存
+rm -rf node_modules
+#提交博客内容
+git add docs/.vuepress/dist
+#提交博客内容
+git commit -m build_blog
+#强推到远程
+git push -f origin deploy
+#切回主干
+git checkout master
