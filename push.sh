650a9605b1175f63ff26f52bfdd53396369df58c
zhanjixun <jixun-zhan@msyc.cc>
2021-04-26 11:23:21 +0800

    里程碑：弃用vuepress,使用手动typora的html

--- push.sh
+++ push.sh
@@ -9,21 +9,22 @@ rm -rf docs/.vuepress/dist
 git pull
 
 git add -A
-git commit -m '脚本提交'
+
+git commit -m 'script push'
 
 git push
 
 ######################### 分开部署到 zhanjixun.gitee.io
 
-npm run docs:build
-
-cd docs/.vuepress/dist
-git init
-git add -A
-git commit -m build
-git remote add origin https://gitee.com/zhanjixun/zhanjixun.git
-git push -f origin master
-
-cd ../../..
-rm -rf node_modules
-rm -rf docs/.vuepress/dist
+#npm run docs:build
+#
+#cd docs/.vuepress/dist
+#git init
+#git add -A
+#git commit -m build
+#git remote add origin https://gitee.com/zhanjixun/zhanjixun.git
+#git push -f origin master
+#
+#cd ../../..
+#rm -rf node_modules
+#rm -rf docs/.vuepress/dist
