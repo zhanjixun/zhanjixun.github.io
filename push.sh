9289dbe1ae1d9c5e3ea0e1611f35667822f6135c
zhanjixun <zhanjixun@qq.com>
2022-10-24 19:52:12 +0800

    script push

--- push.sh
+++ push.sh
@@ -3,8 +3,6 @@ set -e
 set -v
 
 rm -rf .idea
-rm -rf node_modules
-rm -rf docs/.vuepress/dist
 
 git pull
 
@@ -13,18 +11,3 @@ git add -A
 git commit -m 'script push'
 
 git push
-
-######################### 分开部署到 zhanjixun.gitee.io
-
-#npm run docs:build
-#
-#cd docs/.vuepress/dist
-#git init
-#git add -A
-#git commit -m build
-#git remote add origin https://gitee.com/zhanjixun/zhanjixun.git
-#git push -f origin master
-#
-#cd ../../..
-#rm -rf node_modules
-#rm -rf docs/.vuepress/dist
