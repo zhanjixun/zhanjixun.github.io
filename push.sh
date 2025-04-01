fb5fda27bef9da41428da84a6b4ea90a12805891
zhanjixun <zhanjixun@qq.com>
2025-04-01 19:21:01 +0800

    script push

--- push.sh
+++ push.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-set -e
-
-set -v
-
-#sed -i 's#http://zhanjixun\.gitee\.io#/\##g' "assets/Java知识体系.svg"
+set -ev
 
 rm -rf .idea
-
-# 查找未引用的md文件
-# tmpfile=$(mktemp)
-# grep -o 'markdown/[^)]*' _sidebar.md | 
-#   sed 's|markdown/||' | sort > "$tmpfile"
-# find markdown -type f -name '*.md' -exec basename {} .md \; | 
-#   sort | comm -23 - "$tmpfile" | 
-#   sed 's|^|markdown/|;s/$/.md/'
-
-# rm "$tmpfile"
 
 git pull
 
