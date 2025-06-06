13788ad69cbf8b34811bf0edb225ccb1bbac5156
zhanjixun <zhanjixun@qq.com>
2025-06-07 00:06:53 +0800

    修改svg地址替换

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 set -ev
 # rm -rf .idea
 find ./assets/drawio -type f -name "*.bkp" -delete
-sed -i 's#https://zhanjixun\.github\.io##g' assets/index.svg
+sed -i 's#http://localhost:3000##g' assets/index.svg
 git pull
 git add -A
 git commit -m 'script push'
